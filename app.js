dd010f87b4a27e174b6da97c6a1e7f29a8498154
Russell Roberts <russrob23@gmail.com>
2017-01-19 11:37:03 -0800

    First Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// adding a event handler to the click button 
+//getting the input vaules from the input field 
+// adding any new items to our data structure 
+//adding the new data to the UI 
+//We need to calulated the new budget 
+//update the user inferface 
+
+
+//struction our code with modules 
+//	impor
+
+
+// budget Controller 
+const budgetController = (() => {
+
+	
+})();
+
+// UIController 
+const UIController = (() => {
+
+
+})();
+
+
+// Global Controller 
+const controller = ((budgetCtrl, UICtrl) => {
+
+	document.querySelector('.add__btn').addEventListener(click, () => {
+
+		//1. Get the fiel input data 
+		//2. Add the item to the budget controller 
+		//3. Add the item to the budget controller 
+
+	});
+
+})(budgetController, UIController);
